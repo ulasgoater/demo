e4a86c55790c45d9a7daaee141167877802855f3
ulasgoater <ulasneseer@hotmail.com>
2026-08-19 21:57:30 +0200

    Pushing skeleton

--- src/main/java/com/idempotentpayment/account/AccountRepository.java
+++ src/main/java/com/idempotentpayment/account/AccountRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+// JPA stands for Java Persistence API. 
+// It is the standard Java specification for mapping Java objects to database tables
+// and managing database persistence.
 /**
  * Account Repository (DAO Layer).
  * 
